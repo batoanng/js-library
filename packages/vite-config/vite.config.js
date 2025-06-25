5617035b5f7a4df2f5bc284c9bd648e88201ae75
Ba Toan Nguyen <michael.batoan.nguyen@gmail.com>
2025-06-25 23:34:44 +1000

    Fix dep

--- packages/vite-config/vite.config.js
+++ packages/vite-config/vite.config.js
@@ -1,29 +1,12 @@
 const peerDepsExternal = require('@chrisneedham/rollup-plugin-peer-deps-external');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const react = require('@vitejs/plugin-react');
-const path = require('path');
-const { fileURLToPath } = require('url');
 const { defineConfig } = require('vite');
 const dts = require('vite-plugin-dts').default;
 const svgr = require('vite-plugin-svgr').default;
 
 const viteConfig = defineConfig({
   plugins: [peerDepsExternal(), nodeResolve(), react(), dts(), svgr()],
-  build: {
-    target: 'es2022',
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      formats: ['es'],
-      name: 'mui-components',
-      fileName: 'mui-components',
-    },
-    sourcemap: true,
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
 });
 
 module.exports = { viteConfig };
